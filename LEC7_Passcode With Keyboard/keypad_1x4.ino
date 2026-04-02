7a2ad1ba4c92608a35996907a339c9ad88180f99
Zhiyang Jin <z76jin@uwaterloo.ca>
2026-04-02 11:02:49 -0400

    ~

---
+++ LEC7_Passcode With Keyboard/keypad_1x4.ino
@@ -0,0 +1,38 @@
+#include <Keypad.h> // use keypad library
+
+// step 1: define keyboard size
+const int rows = 1;
+const int cols = 4;
+
+// step 2: define keypad layout
+char keys[rows][cols] = {
+    {'1', '2', '3', 'A'}};
+
+// step 3: connect keypad pins
+byte rowPins[rows] = {11};         // connect to row 1
+byte colPins[cols] = {7, 6, 5, 4}; // connect to col 1
+
+// step 4: create keypad object
+Keypad myKeypad = Keypad(makeKeymap(keys), rowPins,
+                         colPins, rows, cols);
+
+void setup()
+{
+    // put your setup code here, to run once
+    Serial.begin(9600);
+    Serial.println("Keypad is ready! Press any key...");
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    // step 5: read key from keyboard
+    char key = myKeypad.getKey();
+
+    // step 6: print the key just read
+    if (key)
+    {
+        Serial.print("Pressed: ");
+        Serial.println(key);
+    }
+}
