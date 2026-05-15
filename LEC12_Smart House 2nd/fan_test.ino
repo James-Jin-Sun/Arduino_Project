7531498de2f00d50dd3c28167f2155f1a3cb7263
Zhiyang Jin <z76jin@uwaterloo.ca>
2026-05-15 13:09:04 -0400

    ~

--- LEC12_Smart House 2nd/fan_test.ino
+++ LEC12_Smart House 2nd/fan_test.ino
@@ -18,6 +18,11 @@ void loop()
     analogWrite(motorPin_2, 0); // half speed cooling fan
     delay(5000);
 
+    // allow fan to slow down before switching direction
+    analogWrite(motorPin_1, 0);
+    analogWrite(motorPin_2, 0); // stop motor: both inputs LOW
+    delay(2000);
+
     analogWrite(motorPin_1, 0);
     analogWrite(motorPin_2, 255); // full speed exhaust fan
     delay(5000);
