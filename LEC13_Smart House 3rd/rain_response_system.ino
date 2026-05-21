e4e52523be2a5a3d5560332681013da8277d26a5
Zhiyang Jin <z76jin@uwaterloo.ca>
2026-05-21 16:31:07 -0400

    ~

--- LEC13_Smart House 3rd/rain_response_system.ino
+++ LEC13_Smart House 3rd/rain_response_system.ino
@@ -1,11 +1,13 @@
-#include <Arduino.h>
+#include <Servo.h> // use Servo library
 
 int rainPin = A1;
 int rainValue = 0;
-int rainThs = 500;
+int lightRainThs = 700; // light rain threshold value (depends on the sensor and environment, adjust as needed)
+int heavyRainThs = 400; // heavy rain threshold value (depends on the sensor and environment, adjust as needed)
 
 int servoPin = 10;
 int winClosDeg = 0;
+int winHalfDeg = 90;
 int winOpenDeg = 180;
 Servo windowServo;
 
@@ -24,15 +26,23 @@ void loop()
     Serial.print("Rain Value: ");
     Serial.println(rainValue);
 
-    if (rainValue > rainThs)
+    if (rainValue > lightRainThs)
     {
-        windowServo.write(winClosDeg); // Close the window
-        Serial.println("Rain Detected! Closing the window.");
-    }
-    else
-    {
+        // No rain
         windowServo.write(winOpenDeg); // Open the window
         Serial.println("No Rain. Keeping the window open.");
+    }
+    else if (rainValue <= lightRainThs && rainValue > heavyRainThs)
+    {
+        // Light rain
+        windowServo.write(winHalfDeg); // Partially open the window
+        Serial.println("Light Rain Detected! Partially opening the window.");
+    }
+    else // rainValue <= heavyRainThs
+    {
+        // Heavy rain
+        windowServo.write(winClosDeg); // Close the window
+        Serial.println("Heavy Rain Detected! Closing the window.");
     }
 
     delay(500);
